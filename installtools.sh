9297a2c40f566456b1301e07339a938de7d1bfdd
Ameya Vijay Unchagaonkar <123241965+aunchagaonkar@users.noreply.github.com>
2023-11-12 19:08:58 +0530

    Update installtools.sh

--- installtools.sh
+++ installtools.sh
@@ -1,2 +1,3 @@
 sudo pacman -Syu
+yay -S google-chrome visual-studio-code-bin archlinux-tweak-tool-git arcolinux-app-glade-git microsoft-edge-stable-bin oneko peazip-qt-bin ttf-anka-coder-condensed update-grub cloudflare-warp-bin
 
