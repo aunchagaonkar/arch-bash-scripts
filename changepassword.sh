d19a4b28532a6a35aa121572a3109ba2f6972870
Ameya Vijay Unchagaonkar <123241965+aunchagaonkar@users.noreply.github.com>
2023-11-15 19:52:57 +0530

    Added comments for better readability in changepassword.sh

--- changepassword.sh
+++ changepassword.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 read -p "Enter the Username (whose password you want to change): " username
 if id "$username" &>/dev/null; then
-    echo "Please enter the new password for $username:"
-    read -s newpassword
-    echo -e "$newpassword\n$newpassword" | sudo passwd "$username"
-    if [ $? -eq 0 ]; then
+    echo "Please enter the new password for $username:" 
+    read -s newpassword #Take input from the user 
+    echo -e "$newpassword\n$newpassword" | sudo passwd "$username" 
+    if [ $? -eq 0 ]; then #Check if there are any errors in previous output
         echo "Password for $username has been changed successfully."
     else
         echo "Failed to change the password for $username."
