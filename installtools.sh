fb60ecc8fe1acc7120bc19608ee11a704dbe848f
Ameya Vijay Unchagaonkar <123241965+aunchagaonkar@users.noreply.github.com>
2023-11-16 23:39:09 +0530

    Added comments in installtools.sh

--- installtools.sh
+++ installtools.sh
@@ -1,3 +1,3 @@
-sudo pacman -Syu
-yay -S google-chrome visual-studio-code-bin archlinux-tweak-tool-git arcolinux-app-glade-git microsoft-edge-stable-bin oneko peazip-qt-bin ttf-anka-coder-condensed update-grub cloudflare-warp-bin
-sudo pacman -S vlc qbittorent
+sudo pacman -Syu #Upgrade all packages
+yay -S google-chrome visual-studio-code-bin archlinux-tweak-tool-git arcolinux-app-glade-git microsoft-edge-stable-bin oneko peazip-qt-bin ttf-anka-coder-condensed update-grub cloudflare-warp-bin #Install essential tools
+sudo pacman -S vlc qbittorent #Install essentials tools using pacman
