439ae452a46fd809795bedf83260baa83d155ae6
Ameya Vijay Unchagaonkar <123241965+aunchagaonkar@users.noreply.github.com>
2023-11-17 18:18:26 +0530

    Update install_yay_for_arch.sh

--- install_yay_for_arch.sh
+++ install_yay_for_arch.sh
@@ -1,5 +1,5 @@
 echo "DO NOT RUN THIS SCRIPT AS ROOT!!!"
-echo "Make sure you are connected to Interner" 
+echo "Make sure you are connected to Internet" 
 sudo pacman -S --needed base-devel git #Required packages for building Yay
 git clone https://aur.archlinux.org/yay.git  #cloned repository | Internet required
 cd yay
