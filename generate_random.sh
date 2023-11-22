07b68880b0676b8773a98924ba013a4d5288e4ad
Ameya Vijay Unchagaonkar <123241965+aunchagaonkar@users.noreply.github.com>
2023-11-22 18:37:37 +0530

    Update generate_random.sh

--- generate_random.sh
+++ generate_random.sh
@@ -1,4 +1,4 @@
 echo "Enter a range: "
 read range #Takes value from user
 echo $RANDOM #Prints a random value
-echo $((RANDOM % $((range+1))))
+echo $((RANDOM % $((range+1)))) #Prints a random number
