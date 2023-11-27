89a2157e3f1b9530ace8076bcd1d8e5802da01fa
Ameya Vijay Unchagaonkar <123241965+aunchagaonkar@users.noreply.github.com>
2023-11-27 14:11:23 +0530

    Update installtools.sh

--- installtools.sh
+++ installtools.sh
@@ -1,5 +1,6 @@
 sudo pacman -Syu #Upgrade all packages
 yay -S google-chrome visual-studio-code-bin archlinux-tweak-tool-git arcolinux-app-glade-git microsoft-edge-stable-bin oneko peazip-qt-bin ttf-anka-coder-condensed update-grub cloudflare-warp-bin #Install essential tools
-sudo pacman -S vlc qbittorent bucklespring tlp scrcpy  #Install essentials tools using pacman
+sudo pacman -S vlc qbittorent tlp scrcpy  #Install essentials tools using pacman
 sudo pacman -S quadrapassel #Install game
 sudo pacman -S pulseaudio-bluetooth #Install if bluetooth is not working properly
+sudo pacman -S bucklespring #Install a fun keyboard tool
