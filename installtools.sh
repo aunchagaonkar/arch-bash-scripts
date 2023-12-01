3da9490eab2e93bb72399a88e17d3f42c02fef65
Ameya Vijay Unchagaonkar <123241965+aunchagaonkar@users.noreply.github.com>
2023-12-01 23:01:31 +0530

    Update installtools.sh

--- installtools.sh
+++ installtools.sh
@@ -1,5 +1,6 @@
 sudo pacman -Syu #Upgrade all packages
-yay -S google-chrome visual-studio-code-bin archlinux-tweak-tool-git arcolinux-app-glade-git microsoft-edge-stable-bin oneko peazip-qt-bin ttf-anka-coder-condensed update-grub cloudflare-warp-bin #Install essential tools
+yay -S google-chrome archlinux-tweak-tool-git arcolinux-app-glade-git microsoft-edge-stable-bin oneko peazip-qt-bin ttf-anka-coder-condensed update-grub cloudflare-warp-bin #Install essential tools
+yay -S visual-studio-code-bin #Installs visual studio code from AUR
 sudo pacman -S vlc #Install essentials tools using pacman
 sudo pacman -S quadrapassel #Install game
 sudo pacman -S pulseaudio-bluetooth #Install if bluetooth is not working properly
