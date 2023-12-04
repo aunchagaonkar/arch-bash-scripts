6d943a4ed1df25749af2d2cc1bd0474f2f6e8441
Ameya Vijay Unchagaonkar <123241965+aunchagaonkar@users.noreply.github.com>
2023-12-04 17:24:09 +0530

    Update installtools.sh

--- installtools.sh
+++ installtools.sh
@@ -1,5 +1,6 @@
 sudo pacman -Syu #Upgrade all packages
-yay -S archlinux-tweak-tool-git arcolinux-app-glade-git microsoft-edge-stable-bin oneko peazip-qt-bin ttf-anka-coder-condensed update-grub cloudflare-warp-bin #Install essential tools
+yay -S archlinux-tweak-tool-git arcolinux-app-glade-git microsoft-edge-stable-bin oneko peazip-qt-bin update-grub cloudflare-warp-bin #Install essential tools
+yay -S ttf-anka-coder-condensed #Installs a best looking font using Yay Package manager
 yay -S visual-studio-code-bin #Installs visual studio code from AUR
 yay -S google-chrome #Installs google chrome on arch linux using Yay package manager
 sudo pacman -S vlc #Install essentials tools using pacman
